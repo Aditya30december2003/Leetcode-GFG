3969400bf8613ba6b7d5fa755f243f2079d73935
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-07-20 18:21:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (11.02%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int count=0;
+        //bits ko 1 se end krne pe agr 1 aega then the bit is one..esa krk bits ko right shifte krdo...ese loop mein saari bits check krlo.
         while(n){
             if((n&1)==1){
                 count+=1;
