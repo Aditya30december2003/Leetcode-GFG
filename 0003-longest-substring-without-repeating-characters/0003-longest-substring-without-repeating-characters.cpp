b31706fff3e43afde2eeb470a626ff7044148037
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-07-22 23:50:54 +0530

    Time: 3 ms (95.39%), Space: 10 MB (86.24%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,10 +6,10 @@ public:
       int first =0 , second=0 , length=0;
       while(second<s.length()){
         while(arr[s[second]]==1){
-            arr[s[first]]=0;
+            arr[s[first]]=0; // character is no longer in the current window
             first++;
         }
-        arr[s[second]]=1;
+        arr[s[second]]=1; // add current character to the window
         length=max(length , second-first+1);
         second++;
       }
