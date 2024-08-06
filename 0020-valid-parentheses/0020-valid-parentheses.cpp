179e2c981b013900c3d63167783efbb4fa139c5d
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-08-06 11:49:40 +0530

    Time: 0 ms (100%), Space: 7.7 MB (61.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,41 +2,40 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>stk;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('||s[i]=='{'||s[i]=='['){ //opening
-              stk.push(s[i]);
+        for(int i=0;i<s.size();i++){// when opening brackets
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                stk.push(s[i]);
             }
-            else{ //closing
-               if(stk.empty()){
-                return 0;
-               }
-               else if(s[i]==')'){
-                if(stk.top()!='('){
-                    return 0;
+            else{
+                if(stk.empty()){
+                    return false;
                 }
-                else{
-                    stk.pop();
+                else if(s[i]==')'){
+                    if(stk.top()=='('){
+                        stk.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
-               }
                 else if(s[i]=='}'){
-                if(stk.top()!='{'){
-                    return 0;
+                    if(stk.top()=='{'){
+                        stk.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
                 else{
-                    stk.pop();
+                    if(stk.top()=='['){
+                        stk.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
-               }
-                else{
-                if(stk.top()!='['){
-                    return 0;
-                }
-                else{
-                    stk.pop();
-                }
-               }
             }
-
         }
-        return stk.empty(); // if empty that means valid
+        return stk.empty();
     }
 };
