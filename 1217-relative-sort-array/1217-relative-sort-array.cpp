a3426d3cdbf6bc7a932afede6c4cd9093e931b7a
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-08-28 23:34:05 +0530

    Time: 6 ms (19.58%), Space: 10.5 MB (29.78%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-       vector<int>ans;
+      vector<int>ans;
 
-       map<int , int>map;
-       for(auto x : arr1)
-       map[x]++;
+      map<int , int>map;
 
-       for(auto x : arr2){
-         if(map.find(x) != map.end()){
-            auto index = map.find(x);
+      for(auto x : arr1){
+        map[x]++;
+      }
 
-            int count = index->second; //5->2
-            vector<int>v(count , x); // 5, 5
-            ans.insert(ans.end() , v.begin() , v.end());
-            map.erase(x);
-         }
+      for(auto x : arr2){
+       if(map.find(x) != map.end()){
+        auto temp = map.find(x);
+       // Here, temp is likely an iterator or pointer to an element in a container like a map. The -> operator is used to access the second member
+        int count = temp->second; // 5 -> 2
+        vector<int>v(count , x); //5 ,5
+        ans.insert(ans.end() , v.begin() , v.end());
+        map.erase(x);
        }
-
-       // for remaining arr1 elements
-       for(auto x : map){
-         int element = x.first;
-         int count = x.second;
-
-         vector<int>v(count , element);
-         ans.insert(ans.end() , v.begin() , v.end());
-       }
-    return ans;
+      }
+      //for remaining
+      for(auto x : map){
+        //In contrast, if you had an iterator or an object directly (not a pointer), you would use the dot (.)
+        int element = x.first;
+        int count = x.second;
+        vector<int>v(count , element);
+        ans.insert(ans.end() , v.begin() , v.end());
+      }
+      return ans;
     }
 };
