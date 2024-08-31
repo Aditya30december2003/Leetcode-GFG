509c18a4402dd3a994644312575eb2d49a08ee1e
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-08-31 12:12:53 +0530

    Time: 11 ms (71.23%), Space: 26.8 MB (59.26%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,33 +10,29 @@
  * };
  */
 class Solution {
-private:
-int find(vector<int>&inorder , int target , int InStart , int InEnd){
-    for(int i=InStart ; i<=InEnd ; i++){
-        if(target == inorder[i]){
+    int find(vector<int>&inorder , int inStart , int inEnd , int target){
+        for(int i=inStart ; i<=inEnd ; i++){
+            if(target == inorder[i])
             return i;
         }
+
+        return -1;
     }
-    return -1;
-}
-TreeNode *Tree(vector<int>&preorder  , vector<int>&inorder , int InStart , int InEnd , int index)
-{
-    if(InStart > InEnd)
-    return NULL;
-    TreeNode* root = new TreeNode(preorder[index]);
+    TreeNode* Tree(vector<int>&preorder, vector<int>&inorder , int inStart , int inEnd , int pre){
+     if(inStart>inEnd)
+     return NULL;
 
-    int pos = find(inorder , preorder[index], InStart , InEnd); //find the root in inorder to divide
-     
-    //left
-    root->left = Tree(preorder , inorder , InStart , pos-1 , index+1);
-    //right
-    root->right=Tree(preorder , inorder , pos+1 , InEnd , index+(pos-InStart)+1 );
+     TreeNode *root = new TreeNode(preorder[pre]);
+     int index = find(inorder , inStart , inEnd , preorder[pre]);
 
-    return root;
-}
+     root->left=Tree(preorder , inorder , inStart , index-1 , pre+1 );
+     root->right = Tree(preorder , inorder , index+1 , inEnd , pre+(index-inStart)+1);
+
+     return root;
+    }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int preIndex=0;
-        return Tree(preorder , inorder , 0 , preorder.size()-1 , preIndex );
+        
+        return Tree(preorder , inorder , 0 , inorder.size()-1 , 0);
     }
 };
