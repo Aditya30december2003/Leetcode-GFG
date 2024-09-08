c6eb714f6b60a48d3c42d09cc7625492d6a03e98
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-08 23:45:56 +0530

    Time: 36 ms (18.34%), Space: 14.7 MB (10.57%) - LeetHub

--- 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
+++ 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    bool isSorted(vector<string>& s , int col){
-        for(int i=0;i<s.size()-1;i++)
-        {
-            if(s[i][col] > s[i+1][col])
-            return false;
+    bool check(vector<string>& strs , int col){
+        for(int i=0;i<strs.size()-1;i++){
+            if(strs[i][col] > strs[i+1][col])
+            return 0;
         }
-        return true;
+        return 1;
     }
     int minDeletionSize(vector<string>& strs) {
         int count=0;
-        int numCol=strs[0].size();
+        int numCol = strs[0].size();
 
         for(int col=0;col<numCol;col++){
-            if(isSorted(strs , col) == 0)
+            if(check(strs , col) ==0)
             count++;
         }
         return count;
