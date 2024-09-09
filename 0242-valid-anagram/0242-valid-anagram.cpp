050e24c541a9fb69f161cdf15bc72bfcb6d3d683
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-09 14:43:01 +0530

    Time: 10 ms (46.04%), Space: 8.8 MB (83.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       if(s.length()!=t.length()){
-        return false;
-       }
-       vector<int>char1(256 , 0);
-       vector<int>char2(256,0);
-       int n=s.length();
-       int i=0;
-       while(i<n){
-        char1[s[i]]++;
-        char2[t[i]]++;
-        i++;
-       }
-       if(char1==char2){
-        return true;
-       }
-       else{
-        return false;
-       }
+       sort(s.begin() , s.end());
+       sort(t.begin() , t.end());
+
+       return s==t; 
     }
 };
