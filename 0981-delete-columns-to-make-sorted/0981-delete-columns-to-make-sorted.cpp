035bb979cd62d9551f2246100ac18758e6d9945b
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-09 11:07:49 +0530

    Time: 37 ms (11.08%), Space: 14.4 MB (99.07%) - LeetHub

--- 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
+++ 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
@@ -3,18 +3,19 @@ public:
     bool check(vector<string>& strs , int col){
         for(int i=0;i<strs.size()-1;i++){
             if(strs[i][col] > strs[i+1][col])
-            return 0;
+            return false;
         }
-        return 1;
+        return true;
     }
     int minDeletionSize(vector<string>& strs) {
+        int numCol=strs[0].size();
         int count=0;
-        int numCol = strs[0].size();
 
         for(int col=0;col<numCol;col++){
-            if(check(strs , col) ==0)
+            if(!check(strs , col))
             count++;
         }
+
         return count;
     }
 };
