7c527857c30faf9e9879df2434be2df5d215288a
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-09 10:53:05 +0530

    Time: 4 ms (57.23%), Space: 10.3 MB (47.66%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,28 +3,30 @@ public:
     string reverseWords(string s) {
         string ans="";
         stack<string>stk;
-        
+
         int i=0;
+
         while(i<s.size()){
-         string word="";
+            string word="";
 
-         while(i<s.size() && s[i]!=' '){
-            word+=s[i];
+            while(i<s.size() && s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+
+            if(!word.empty()){
+                stk.push(word);
+            }
             i++;
-         }
-
-
-         if(!word.empty())
-         stk.push(word);
-         i++;
         }
 
         while(!stk.empty()){
-           ans+=stk.top() ;
-            stk.pop();
-            if (!stk.empty()) {
-                ans += " ";
-            }
+           ans+=stk.top();
+           stk.pop();
+
+           if(!stk.empty()){
+            ans+=" ";
+           }
         }
 
         return ans;
