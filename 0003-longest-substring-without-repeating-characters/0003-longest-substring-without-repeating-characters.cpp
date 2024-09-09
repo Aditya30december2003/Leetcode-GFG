1dc39dfc079010d9d02689c8e43676294797661d
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-10 00:03:17 +0530

    Time: 0 ms (100%), Space: 10.1 MB (95.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-      if(s.length()==0) return 0;
-      vector<bool> arr(256 , 0);
-      int first =0 , second=0 , length=0;
-      while(second<s.length()){
-        while(arr[s[second]]==1){
-            arr[s[first]]=0; // character is no longer in the current window
-            first++;
+        if(s.size()==0) return 0;
+
+        int first=0 , second=0 , length=0;
+        vector<bool>arr(256,0);
+        while(second<s.size()){
+            while(arr[s[second]]==1){
+                arr[s[first]]=0;
+                first++;
+            }
+            arr[s[second]]=1;
+            length = max(length , second-first+1);
+            second++;
         }
-        arr[s[second]]=1; // add current character to the window
-        length=max(length , second-first+1);
-        second++;
-      }
-      return length;
+        return length;
     }
 };
