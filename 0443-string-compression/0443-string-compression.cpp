038de7fcffaa967a7dd1be804ed7e08e8de229d6
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-09 17:37:54 +0530

    Time: 4 ms (70.4%), Space: 14 MB (68.26%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,29 +3,29 @@ public:
     int compress(vector<char>& chars) {
         int n=chars.size();
 
-        int index =0;
+        int index=0;
         int i=0;
+
         while(i<n){
-         char curr_char = chars[i];
-         int count=0;
-         // reprating elements
+            char curr_char=chars[i];
+            int count=0;
 
-         while(i<n && chars[i]==curr_char){
-            count++;
-            i++;
-         }
-
-        chars[index] = curr_char;
-        index++; //char is assigned
-
-        //now assign number
-        if(count>1){
-            string count_str=to_string(count);
-            for(char &ch:count_str){
-                chars[index]=ch;
-                index++;
+            while(i<n && chars[i]==curr_char){
+                count++;
+                i++;
             }
-        }
+
+            chars[index]=curr_char;
+            index++;
+
+            //assign freq
+            if(count>1){
+                string count_str=to_string(count);
+                for(char &ch:count_str){
+                    chars[index]=ch;
+                    index++;
+                }
+            }
 
         }
 
