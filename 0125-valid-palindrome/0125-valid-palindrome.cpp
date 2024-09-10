ed38973c2a4f84306352e27f2f9248bad576b0e1
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-10 11:26:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (20.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-      string ans="";
-      int first=0 , second=0;
-      while(second<s.size()){
-        if(isalnum(s[second])){
-          ans.push_back(tolower(s[second]));
-          first++;
-        }
-        second++;
-      }
-      int start=0 , end=ans.size()-1;
-      while(start<end){
-        if(ans[start]!=ans[end]){
+    bool check(string s){
+        int start=0 , end=s.size()-1;
+        while(start<=end){
+            if(s[start]!=s[end])
             return false;
+
+            start++;
+            end--;
         }
-        start++;
-        end--;
-      }
-      return true;
+        return true;
+    }
+    bool isPalindrome(string s) {
+        string str="";
+        for(int i=0 ;i<s.size();i++){
+            if(isalpha(s[i]) || isdigit(s[i])){
+                str+=tolower(s[i]);
+            }
+        }
+
+        return check(str);
     }
 };
