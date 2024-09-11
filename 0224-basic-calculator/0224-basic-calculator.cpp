ce0bfcd4330b6b261cfb68bd02edab4b16cbf421
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-11 23:25:45 +0530

    Time: 9 ms (58.53%), Space: 9.5 MB (70.15%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -2,58 +2,55 @@ class Solution {
 public:
     int calculate(string s) {
         stack<int>stk;
-
         int result=0;
         int number=0;
-        int sign =1;
+        int sign  =1;
 
         for(int i=0;i<s.size();i++){
-            // if is digit
-            if(isdigit(s[i])){
-                number = (number*10) + (s[i]-'0');
-            }
-            // if + sign
-            else if(s[i] == '+'){
-                // number is made
-                // now add it to result
-                result+=(sign*number);
-                number=0;
-                sign=1;
-            }
-            // if - sign
-            else if(s[i] == '-'){
-                //number is made
-                //now add it to result
-                result+=(sign*number);
-                number=0;
-                sign=-1;
-            }
-            // if ( sign
-            else if(s[i] == '('){
-                // now solve all this is in ()
-                stk.push(result);
-                stk.push(sign);
-                // initializing for bracket
-                result=0;
-                number=0;
-                sign=1;
-            }
-            // if ) sign
-            else if(s[i] == ')'){
-                // now bracket is closed so evaluate
-                result += number*sign;
-                number=0;
+          //digit
+          if(isdigit(s[i])){
+            number=(number*10) +(s[i]-'0');
+          }
+          // + sign
+          else if(s[i] == '+'){
+            // number bn gya hai
+            result+=(number*sign);
+            number=0;
+            sign=1;
+          }
+          // - sign
+          else if(s[i] == '-'){
+            // number bn gya hai
+            result+=(number*sign);
+            number=0;
+            sign=-1;
+          }
+          // ( sign
+          else if(s[i] == '('){
+            //now repeat the whole procedure in ( all over again
+            stk.push(result);
+            stk.push(sign);
 
+            result=0;
+            number=0;
+            sign=1;
+          }
+          // ) sign
+          else if(s[i]==')'){
+            //now evaluate the whole pprocess
+            result+=number*sign;
+            number=0;
+            
+            int stk_sign=stk.top();stk.pop();
+            int stk_num = stk.top();stk.pop();
 
-                int stk_sign=stk.top();stk.pop();
-                int stk_num=stk.top();stk.pop();
-
-                result*=stk_sign;
-                result+=stk_num;
-            }
+            result*=stk_sign;
+            result+=stk_num;
+          }
         }
-        //add the ending num
-        result+=number*sign;
+
+        // last num
+        result+=(sign*number);
         return result;
     }
 };
