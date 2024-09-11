b46c25d58f6489d585354076464ca065350afc31
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-11 10:08:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,46 +1,55 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<int>stk;
-       for(int i=0;i<s.size();i++){
-        if(stk.empty()){
-            if(s[i]==')'||s[i]=='}'||s[i]==']'){
-                return false;
+        if(s.size()==0) return 1;
+        stack<char>stk;
+        int i=0;
+        while(i<s.size()){
+            if(stk.empty()){
+                if(s[i]==')' || s[i]=='}' || s[i]==']'){
+                    return false;
+                }
+                else{
+                    stk.push(s[i]);
+                }
             }
+
             else{
-                stk.push(s[i]);
-            }
-        }
-        else if(s[i]=='('||s[i]=='{'||s[i]=='['){
-           stk.push(s[i]); 
-        }
-        else{
-            if(s[i]==')'){
                 if(stk.top()=='('){
-                    stk.pop();
+                    if(s[i]==')')
+                    {
+                        stk.pop();
+                    }
+                    else{
+                        stk.push(s[i]);
+                    }
                 }
+                else if(stk.top()=='{'){
+                    if(s[i]=='}')
+                    {
+                        stk.pop();
+                    }
+                    else{
+                        stk.push(s[i]);
+                    }
+                }
+                
                 else{
-                    return 0;
+                    if(s[i]==']')
+                    {
+                        stk.pop();
+                    }
+                    else{
+                        stk.push(s[i]);
+                    }
                 }
             }
-            else if(s[i]=='}'){
-                if(stk.top()=='{'){
-                    stk.pop();
-                }
-                else{
-                    return 0;
-                }
-            }
-            else{
-                if(stk.top()=='['){
-                    stk.pop();
-                }
-                else{
-                    return 0;
-                }
-            }
+            i++;
         }
-    }
-       return stk.empty(); 
+
+        if(stk.empty())
+        return true;
+
+        return false;
     }
 };
