11b8af078824620def59d571b6b4eb8a721655f4
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-11 11:06:35 +0530

    Time: 4 ms (25.32%), Space: 8.3 MB (12.71%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,28 +1,33 @@
 class Solution {
-private:
-string netString(string s){
-    stack<char>stk;
-    for(int i=0;i<s.size();i++){
-       if(s[i]=='#'){
-        if(!stk.empty()){
-            stk.pop();
-        }
-       }
-       else{
-        stk.push(s[i]);
-       }
-    }
-    string ans="";
-        while(!stk.empty()){
-            ans+=stk.top();
-            stk.pop();
-        }
-        return ans;
-}
 public:
+    void Convert(string&str , string&c){
+     stack<char>stk;
+
+     int i=0;
+     while(i<str.size()){
+        if(str[i]=='#'){
+            if(!stk.empty()){
+             stk.pop();
+            }
+        }
+        else{
+            stk.push(str[i]);
+        }
+        i++;
+     }
+     while(!stk.empty()){
+        c+=stk.top();
+        stk.pop();
+     }
+    
+    }
     bool backspaceCompare(string s, string t) {
-       string trueS=netString(s);
-       string trueT=netString(t);
-       return trueS==trueT; 
+        string s1="";
+        string t1="";
+
+        Convert(s , s1);
+        Convert(t , t1);
+
+        return s1 == t1;
     }
 };
