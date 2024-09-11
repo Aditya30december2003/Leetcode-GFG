95d3214f9bad62400830346312d535cb8462332d
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-11 14:04:45 +0530

    Time: 5 ms (83.24%), Space: 9.6 MB (51.76%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -2,56 +2,58 @@ class Solution {
 public:
     int calculate(string s) {
         stack<int>stk;
+
         int result=0;
         int number=0;
-        int sign =1; // shuru mein +
+        int sign =1;
 
         for(int i=0;i<s.size();i++){
-            // digit 
+            // if is digit
             if(isdigit(s[i])){
-                // make the number
-                number=number*10 + (s[i]-'0');
+                number = (number*10) + (s[i]-'0');
             }
-            // + sign
+            // if + sign
             else if(s[i] == '+'){
-                // number bn chuka hai
-                //result mein add kro
-                result += (number*sign);
+                // number is made
+                // now add it to result
+                result+=(sign*number);
                 number=0;
-                sign=1;  // + sign
+                sign=1;
             }
-            // - sign
+            // if - sign
             else if(s[i] == '-'){
-                //number bn chuka hai
-                // result mein add kro
-                result += (number*sign);
+                //number is made
+                //now add it to result
+                result+=(sign*number);
                 number=0;
                 sign=-1;
             }
-            // ( sign
-            else if(s[i] == '('){ // ab bracket k andar ka calc kro
+            // if ( sign
+            else if(s[i] == '('){
+                // now solve all this is in ()
                 stk.push(result);
                 stk.push(sign);
-                // wapis jo krre hai bracket k andar chalu kro
+                // initializing for bracket
                 result=0;
                 number=0;
                 sign=1;
             }
-            // ) sign
+            // if ) sign
             else if(s[i] == ')'){
-                result += (number*sign);
+                // now bracket is closed so evaluate
+                result += number*sign;
                 number=0;
 
-                int stk_sign = stk.top();stk.pop();
-                int last_result = stk.top();stk.pop();
+
+                int stk_sign=stk.top();stk.pop();
+                int stk_num=stk.top();stk.pop();
 
                 result*=stk_sign;
-                result+=last_result;
+                result+=stk_num;
             }
         }
-
-        // now add the last number 1 + (2+3) - 4 // here -4 is to add
-        result+= number*sign;
+        //add the ending num
+        result+=number*sign;
         return result;
     }
 };
