427524aaa175e1e9a0ba8315938313e9fbd5509f
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-15 18:20:24 +0530

    Time: 11 ms (71.24%), Space: 26.8 MB (85.34%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,26 +10,28 @@
  * };
  */
 class Solution {
-    int find(vector<int>& inorder , int inStart , int inEnd , int target){
-        for(int i=inStart ; i<=inEnd ; i++){
-            if(inorder[i] == target)
+public:
+    int find(vector<int>& inorder, int inStart ,int inEnd , int target){
+        for(int i=inStart;i<=inEnd;i++){
+            if(inorder[i]==target)
             return i;
         }
         return -1;
     }
-    TreeNode* Tree(vector<int>&preorder, vector<int>&inorder , int inStart , int inEnd , int pre){
-       if(inStart>inEnd)
-       return NULL;
+    TreeNode* Tree(vector<int>& preorder, vector<int>& inorder, int inStart ,int inEnd , int preIndex){
+        if(inStart>inEnd)
+        return NULL;
 
-       TreeNode *root = new TreeNode(preorder[pre]);
-       int index = find(inorder , inStart , inEnd , preorder[pre]);
-       root->left = Tree(preorder , inorder , inStart , index-1 , pre+1);
-       root->right = Tree(preorder , inorder , index+1 , inEnd , pre+(index-inStart)+1);
+        TreeNode *root= new TreeNode(preorder[preIndex]);
 
-       return root; 
+        int index = find(inorder,inStart,inEnd, preorder[preIndex] );
+
+        root->left = Tree(preorder , inorder , inStart , index-1 , preIndex+1);
+        root->right= Tree(preorder , inorder, index+1 , inEnd , preIndex+(index-inStart)+1);
+
+        return root;
     }
-public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return Tree(preorder , inorder , 0 , inorder.size()-1 , 0);
+       return Tree(preorder , inorder , 0 , inorder.size()-1 , 0); 
     }
 };
