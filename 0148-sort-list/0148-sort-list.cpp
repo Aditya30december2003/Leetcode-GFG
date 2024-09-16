90bb3d504ac8153e1703f6996863d524af56fde8
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-09-16 10:23:40 +0530

    Time: 129 ms (63.51%), Space: 58.1 MB (83.48%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,40 +10,19 @@
  */
 class Solution {
 public:
-    ListNode* Mid(ListNode*head){
-        ListNode*slow=head , *fast=head;
-        while(fast && fast->next&& fast->next->next){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* mergeSortedList(ListNode* p , ListNode* q){
-     if(!p)
-     return q;
-     if(!q)
-     return p;
-     if(p->val <= q->val)
-     {
-        p->next = mergeSortedList(p->next , q);
-        return p;
-     }
-     if(q->val < p->val){
-        q->next = mergeSortedList(p,q->next);
-        return q;
-     }
-
-     return NULL;
-    }
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode* mid = Mid(head);
-        ListNode* newhead=mid->next;
-        mid->next=NULL;
-
-        ListNode* left_half=sortList(head);
-        ListNode* right_half=sortList(newhead);
-
-        return mergeSortedList(left_half , right_half);
+        vector<int>arr;
+        ListNode* temp=head;
+        while(temp){
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(arr.begin() , arr.end());
+        temp=head;
+        for(int i=0;i<arr.size();i++){
+            temp->val=arr[i];
+            temp=temp->next;
+        }
+        return head;
     }
 };
