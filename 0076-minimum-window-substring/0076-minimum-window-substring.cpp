a1812c54664cafe78a6d5fddc2a6781f0543f7e7
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-10-09 10:49:28 +0530

    Time: 20 ms (61.96%), Space: 10.9 MB (63.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char , int>umap;
-        for(char&ch:t){
-            umap[ch]++;
-        }
+        unordered_map<char , int>umap; // char of t-->freq
         int total=t.size();
-        int start=0 , end=0 , ans=INT_MAX; // min lenght if substr
-        int index=-1; //starting index of sub stirng
+        int start=0 , end=0 , index=-1 , ans=INT_MAX;
+        for(char&ch:t)
+        umap[ch]++;
+
         while(end<s.size()){
             umap[s[end]]--;
             if(umap[s[end]]>=0)
             total--;
 
-            // can we decerease the size of widow
+            //make  window size is small
             while(total==0 && start<=end){
                 if(ans> end-start+1){
                   ans=end-start+1;
                   index=start;
                 }
-
+            
                 umap[s[start]]++;
                 if(umap[s[start]]>0)
                 total++;
 
                 start++;
-            }
 
+            }
             end++;
         }
+      if(index==-1)
+      return "";
 
-        if(index==-1)
-        return "";
-        string str="";
-        
-    return s.substr(index ,ans);
+      return s.substr(index , ans);
+      
     }
 };
