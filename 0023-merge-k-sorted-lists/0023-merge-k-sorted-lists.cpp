f3c2c7434d83b2c482dfe8d32c46112176fec597
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-10-20 13:36:38 +0530

    Time: 6 ms (98.53%), Space: 19.4 MB (9.93%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -13,21 +13,20 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         priority_queue<pair<int , ListNode*> , vector<pair<int , ListNode*>> , greater<pair<int , ListNode*>>>q;
         for(int i=0;i<lists.size();i++){
-            if(lists[i])
-            q.push({lists[i]->val , lists[i]});
+            if(lists[i]){
+              q.push({lists[i]->val , lists[i]});
+            }
         }
 
         ListNode* ans=new ListNode(-1);
-        ListNode*temp=ans;
-
+        ListNode* temp=ans;
         while(!q.empty()){
             auto it=q.top();
             q.pop();
-
-            if(it.second->next)
-            q.push({it.second->next->val , it.second->next});
-
-            temp->next=it.second;
+            if(it.second->next){
+                q.push({it.second->next->val , it.second->next});
+            }
+            temp->next=new ListNode(it.first);
             temp=temp->next;
         }
         return ans->next;
