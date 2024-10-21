2ebb774469c238e3ed90086118b532d6fe2e5f40
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-10-21 23:35:43 +0530

    Time: 3 ms (92.75%), Space: 18.7 MB (20.56%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -8,38 +8,32 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
-//  */   //0  -->1
-//         1
-//     1       2 -->2
-//     2       3
-//   3|  4|  5|  6| -->4  width = max(max ,right-left+1);
-//   4   5   6   7
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         if(!root) return 0;
-        
-        queue<pair<TreeNode* , long long>>q;
+        queue<pair<TreeNode* , long long >>q;
         q.push({root , 0});
-        unsigned long long width=0;
+        unsigned long long  maxWidth=0;
         while(!q.empty()){
-            unsigned long long n=q.size();
-            unsigned long long left=q.front().second;
-            unsigned long long right=q.back().second;
-            width=max(width , right-left+1);
+            int n=q.size();
+            unsigned long long  l=q.front().second;
+            unsigned long long  r=q.back().second;
+            maxWidth=max(maxWidth , r-l+1);
+
             while(n--){
-              TreeNode *temp=q.front().first;
-              unsigned long long index=q.front().second;
-              q.pop();
+                TreeNode* temp=q.front().first;
+                unsigned long long  idx=q.front().second;
+                q.pop();
 
+                if(temp->left)
+                q.push({temp->left , 2*idx+1});
 
-              if(temp->left)
-              q.push({temp->left , 2*index+1});
-
-              if(temp->right)
-              q.push({temp->right , 2*index+2});
+                if(temp->right)
+                q.push({temp->right , 2*idx+2});
             }
         }
-        return width;
+        return maxWidth;
     }
 };
