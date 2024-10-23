754963cfcfc8678fa819aa3c0aedfadf37d47247
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-10-23 10:03:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -21,23 +21,23 @@ class Solution
     {
         // code here
         if(low<high){
-            int mid=partition(arr,low,high);
-            quickSort(arr,low,mid-1);
-            quickSort(arr,mid+1,high);
+            int mid=partition(arr , low , high);
+            
+            quickSort(arr , low , mid-1);
+            quickSort(arr , mid+1 , high);
         }
     }
     
     public:
     int partition (int arr[], int low, int high)
     {
-       // Your code here
        int pivot=arr[high];
        int i=low-1;
        
        for(int j=low;j<high;j++){
            if(arr[j]<pivot){
                i++;
-               swap(arr[i],arr[j]);
+               swap(arr[i] , arr[j]);
            }
        }
        swap(arr[i+1] , arr[high]);
