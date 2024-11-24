692f48b61caff5b4846ef98fde61fd6884f5609b
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-11-24 22:59:06 +0530

    Time: 34 ms (21.74%), Space: 53.1 MB (12.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        vector<bool>t(n , false);
+        vector<int>t(n,false);
 
         t[0]=true;
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             for(int j=i-1;j>=0;j--){
-                if(t[j]==true && j+nums[j]>=i)
-                {
+                if(t[j]==true && j+nums[j]>=i){
                     t[i]=true;
                     break;
                 }
