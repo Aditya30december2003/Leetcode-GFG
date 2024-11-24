4fb9f4d71945a59b5d7b31c98d143621c12c9126
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-11-24 23:11:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,15 +3,9 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>> t(n+1 , vector<int>(m+1));
-        
-        //first row and col will be 0
-        for(int i=0;i<n+1;i++){
-            t[i][0]=0;
-        }
-        for(int j=0;j<m+1;j++){
-            t[0][j]=0;
-        }
+        vector<vector<int>>t(n+1 , vector<int>(m+1));
+        for(int i=0;i<n+1;i++) t[i][0]=0;
+        for(int j=0;j<m+1;j++) t[0][j]=0;
 
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
@@ -19,10 +13,10 @@ public:
                     t[i][j]=1+t[i-1][j-1];
                 }
                 else{
-                    t[i][j]=max(t[i][j-1] , t[i-1][j]);
+                    t[i][j]=max(t[i-1][j] , t[i][j]);
                 }
             }
         }
-        return t[n][m];
+    return t[n][m];
     }
 };
