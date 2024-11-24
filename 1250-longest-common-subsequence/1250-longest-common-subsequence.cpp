8cbe6caa78ba07900d2ce3c7d74e5e506543d0eb
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-11-24 23:12:13 +0530

    Time: 26 ms (69.27%), Space: 25.8 MB (68.55%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -13,7 +13,7 @@ public:
                     t[i][j]=1+t[i-1][j-1];
                 }
                 else{
-                    t[i][j]=max(t[i-1][j] , t[i][j]);
+                    t[i][j]=max(t[i-1][j] , t[i][j-1]);
                 }
             }
         }
