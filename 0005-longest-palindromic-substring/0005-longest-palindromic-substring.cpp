a6b3c2243868f5cbb84e59bc1ebc1b69a6a332d1
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-11-25 09:44:26 +0530

    Time: 190 ms (28.37%), Space: 12.8 MB (63.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,54 +1,32 @@
 class Solution {
 public:
+    int t[1001][1001];
+    bool solve(string&s , int i , int j){
+        if(i>=j)
+        return true;
+
+        if(t[i][j]!=-1)
+        return t[i][j];
+
+        if(s[i]==s[j])
+        return t[i][j]=solve(s , i+1 , j-1);
+
+        return t[i][j]=0;
+    }
     string longestPalindrome(string s) {
-        // we have to keep tab if palindrome is of even length or odd length.
-     if(s.length()<=1){
-        return s;
-     }
-     int max_len=1; // max length is initialized to 1
-     int start = 0 , end = 0;
-     int n=s.length();
-     // for odd length of palindrom--> there will be a middle letter
-     for(int i=0;i<n-1;i++){
-        int l=i , r=i;
-        while(l>=0 && r<n ){
-            if(s[l]==s[r]){
-                l-- , r++;
-            }
-            else{
-                break; //break the loop
-            }
+        memset(t , -1 , sizeof(t));
+        int maxLen=INT_MIN;
+        int start=0;
+        for(int i=0;i<s.size();i++){
+          for(int j=i;j<s.size();j++){
+              if(solve(s , i , j)){
+                if(j-i+1>maxLen){
+                    maxLen=j-i+1;
+                    start=i;
+                }
+              }
+          }
         }
-        int len=r-l-1; // this will be the length 
-        if(len>max_len){
-            max_len=len;
-            start=l+1;  // we have to keep track of start of palindrome
-            end = end-1; // end of palindrome
-        }
-
-     }
-
-    //Even length
-    for(int i=0;i<n-1;i++){
-        int l=i , r=i+1;
-        while(l>=0 && r<n ){
-            if(s[l]==s[r]){
-                l-- , r++;
-            }
-            else{
-                break; //break the loop
-            }
-        }
-        int len=r-l-1; // this will be the length 
-        if(len>max_len){
-            max_len=len;
-            start=l+1;  // we have to keep track of start of palindrome
-            end = end-1; // end of palindrome
-        }
-
-     }
-
-     return s.substr(start , max_len);
-
+        return s.substr(start , maxLen);
     }
 };
