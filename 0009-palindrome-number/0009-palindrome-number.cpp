1560d64d9ffe79e8a439bb9bc040a18c0c1577b9
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-11-26 11:05:14 +0530

    Time: 17 ms (5.38%), Space: 12.1 MB (7.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0)
+       if(x<0)
+       return false;
+
+       vector<int>arr;
+       while(x>=1){
+        arr.push_back(x%10);
+        x/=10;
+       } 
+       int s=0 , e=arr.size()-1;
+       while(s<e){
+        if(arr[s]!=arr[e])
         return false;
 
-        long long reverse=0;
-        long long num=x;
-        while(num>=1){
-         int ones = num%10;
-         reverse = reverse*10 + ones;
-
-         num/=10;
-        }
-
-        
-        return reverse == x;
+        s++;
+        e--;
+       }
+       return true;
     }
 };
